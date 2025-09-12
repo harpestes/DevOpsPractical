755713c9ccc8f22adaabf02e3bafb17a17ddeca8
Dykalo Andrij <dikaloandrij@gmail.com>
2025-09-12 08:38:02 +0300

    feat: Add input_product function

---
+++ pw1/main.py
@@ -0,0 +1,14 @@
+def input_product():
+    products = {}
+    while True:
+        name = input("Enter product name or 'stop' to exit: ")
+        if name.lower() == 'stop':
+            break
+        price = float(input("Enter product price: "))
+        stock = int(input("Enter product stock on warehouse: "))
+        products[name] = {'Prise': price, 'Stock': stock}
+
+    return products
+
+products = input_product()
+print(products)
