381614f1dabfd8f5d51492bbb70220a0116d1212
Dykalo Andrij <dikaloandrij@gmail.com>
2025-09-12 08:53:20 +0300

    feat: Add find_product_by_name function

--- pw1/main.py
+++ pw1/main.py
@@ -39,3 +39,19 @@ def print_product_names(product_names, index=0):
 
 product_names = list(products.keys())
 print_product_names(product_names)
+
+def find_product_by_name(products, product_name, index=0):
+    product_keys = list(products.keys())
+
+    if index >= len(product_keys):
+        print("Product not found.")
+        return
+
+    if product_keys[index] == product_name:
+        product_info = products[product_name]
+        print(f"Product found: Price - {product_info['Price']}, Stock - {product_info['Stock']}")
+        return
+
+    find_product_by_name(products, product_name, index + 1)
+
+find_product_by_name(products, str(input("Enter product name to find: ")))
