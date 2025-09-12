964e56edda6e08efa2c15ec27ecd86eb3faad32b
Dykalo Andrij <dikaloandrij@gmail.com>
2025-09-12 08:47:34 +0300

    feat: Add calculate_discount function for products with low stock

--- pw1/main.py
+++ pw1/main.py
@@ -6,7 +6,7 @@ def input_product():
             break
         price = float(input("Enter product price: "))
         stock = int(input("Enter product stock on warehouse: "))
-        products[name] = {'Prise': price, 'Stock': stock}
+        products[name] = {'Price': price, 'Stock': stock}
 
     return products
 
@@ -15,7 +15,18 @@ print(products)
 
 def calculate_stock_value(product_info):
     for product, details in product_info.items():
-        stock_value = details['Stock'] * details['Prise']
-        print(f"Вартість залишку для {product}: {stock_value}")
+        stock_value = details['Stock'] * details['Price']
+        print(f"Stock value for {product}: {stock_value}")
 
 calculate_stock_value(products)
+
+def calculate_discount(product_info):
+    for product, details in product_info.items():
+        if details['Stock'] < 10:
+            discount = details['Price'] * 0.05  # 5% знижка
+            print(f"Discount for {product}: {discount} ({details['Price']} - 5%)")
+            print(f"Total price for {product}: {details['Price'] - discount}")
+        else:
+            print(f"For {product} discount not applied, because stock is {details['Stock']} items.")
+
+calculate_discount(products)
