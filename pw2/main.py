145a51d62829ac99455280e6c0115288f1b44700
Dykalo Andrij <dikaloandrij@gmail.com>
2025-09-26 08:45:43 +0300

    feat: Use StudentFinder class in main script

--- pw2/main.py
+++ pw2/main.py
@@ -1,12 +1,19 @@
 from JsonReader import JsonReader
 from CsvToJsonConverter import CsvToJsonConverter
+from StudentFinder import StudentFinder
 
 if __name__ == "__main__":
     csv_url = "https://informer.com.ua/dut/python/import/st_gt.csv"
-    json_path = "students_data.json"  # Шлях до файлу, куди буде збережено JSON
+    json_path = "students_data.json"
 
     converter = CsvToJsonConverter()
     converter.read_and_convert(csv_url, json_path)
 
     json_reader = JsonReader()
-    json_reader.display_data(json_path)
+    # json_reader.display_data(json_path)
+
+    finder = StudentFinder(json_reader)
+    finder.load_data(json_path)
+    surname_to_find = "Барченко"  # Вкажіть прізвище для пошуку
+    found_students = finder.find_students_by_surname(surname_to_find)
+    finder.display_students_info(found_students)
