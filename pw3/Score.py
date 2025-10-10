089b92024f0dd8f8219771b5495573ca3e41e390
Dykalo Andrij <dikaloandrij@gmail.com>
2025-10-10 23:31:52 +0300

    feat: Implement Score class with show_text method

---
+++ pw3/Score.py
@@ -0,0 +1,13 @@
+class Score:
+    def __init__(self, canvas):
+        self.canvas = canvas
+        self.score = 0
+        self.lost = 0
+        self.text = ""
+        self.show_text()
+
+    def show_text(self):
+        if (self.text == ""):
+            self.text = self.canvas.create_text(350, 10, text=f"Спіймав: 0 Пропустив: 0", font=('Helvetica', 16))
+        else:
+            self.canvas.itemconfig(self.text, text=f"Спіймав: {self.score} Пропустив: {self.lost}")
