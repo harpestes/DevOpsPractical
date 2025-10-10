1989ece9e4884d2faaf42039239d9728214a0c7b
Dykalo Andrij <dikaloandrij@gmail.com>
2025-10-10 23:29:14 +0300

    feat: Implement main.py with basic tkinter window

---
+++ pw3/main.py
@@ -0,0 +1,14 @@
+from tkinter import *
+import random
+import time
+
+tk = Tk()
+tk.title("Гра: Ловець!")
+tk.resizable(0, 0)
+tk.wm_attributes("-topmost", 1)
+
+canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
+canvas.pack()
+
+tk.update()
+time.sleep(3)
