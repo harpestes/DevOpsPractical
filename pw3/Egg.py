0f8b34e761a3017fb8e0ed3e5490d39ba2b5b289
Dykalo Andrij <dikaloandrij@gmail.com>
2025-10-10 23:50:20 +0300

    feat: Implement draw method in Egg class to animate eggs and count lost eggs

--- pw3/Egg.py
+++ pw3/Egg.py
@@ -8,3 +8,13 @@ class Egg:
         self.id = canvas.create_oval(0, 0, 25, 25, fill=color)
         self.canvas.move(self.id, random.randint(10, 490), -10)
         self.y = random.randint(1, 4)
+
+    def draw(self):
+        self.canvas.move(self.id, 0, self.y)
+
+        pos = self.canvas.coords(self.id)
+        if pos[3] >= self.canvas.winfo_height():
+            self.score.lost_egg()
+            self.canvas.delete(self.id)
+            return 'hit bottom'
+        return None
