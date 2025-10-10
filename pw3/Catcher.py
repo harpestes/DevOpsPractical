69dfd2ba15183d633f345e35c289ecc741a98418
Dykalo Andrij <dikaloandrij@gmail.com>
2025-10-10 23:58:05 +0300

    feat: Implement turn_left, turn_right and draw methods Catcher class to move and redraw catcher

--- pw3/Catcher.py
+++ pw3/Catcher.py
@@ -6,3 +6,22 @@ class Catcher:
         self.canvas.move(self.id, 200, 350)
         self.x = 0
         self.canvas_width = self.canvas.winfo_width()
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+
+    def turn_left(self, evt):
+        if self.canvas.coords(self.id)[0] > 0:
+            self.x = -20
+
+    def turn_right(self, evt):
+        if self.canvas.coords(self.id)[2] >= self.canvas_width:
+            self.x = 20
+
+    def draw(self):
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+
+        if pos[0] <= 0:
+            self.x = 0
+        elif pos[2] >= self.canvas_width:
+            self.x = 0
