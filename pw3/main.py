f2d823c6d21a8c2114706d04d9216de1a5d652a2
Dykalo Andrij <dikaloandrij@gmail.com>
2025-10-10 23:40:04 +0300

    feat: Use Score class in main.py

--- pw3/main.py
+++ pw3/main.py
@@ -2,6 +2,8 @@ from tkinter import *
 import random
 import time
 
+from pw3.Score import Score
+
 tk = Tk()
 tk.title("Гра: Ловець!")
 tk.resizable(0, 0)
@@ -9,6 +11,10 @@ tk.wm_attributes("-topmost", 1)
 
 canvas = Canvas(tk, width=500, height=400, bd=0, highlightthickness=0)
 canvas.pack()
+
+score = Score(canvas)
+tk.update()
+time.sleep(1)
 
 tk.update()
 time.sleep(3)
