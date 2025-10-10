b21e356241a616c2196b6f694b46e192356e091e
Dykalo Andrij <dikaloandrij@gmail.com>
2025-10-10 23:48:49 +0300

    feat: Implement infinite basic game cycle

--- pw3/main.py
+++ pw3/main.py
@@ -1,7 +1,9 @@
+import random
 from tkinter import *
 import time
 
 from pw3.Catcher import Catcher
+from pw3.Egg import Egg
 from pw3.Score import Score
 
 tk = Tk()
@@ -14,5 +16,14 @@ canvas.pack()
 
 score = Score(canvas)
 catcher = Catcher(canvas, 'blue', score)
+
+eggs = []
+while 1:
+    if random.randint(1, 100) == 1:
+        eggs.append(Egg(canvas, 'red', score))
+    tk.update_idletasks()
+    tk.update()
+    time.sleep(0.01)
+
 tk.update()
 time.sleep(1)
