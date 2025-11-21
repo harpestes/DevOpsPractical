6a0e8dbaa03d07c3fb7945e8d696495348469f97
harpestes <dikaloandrij@gmail.com>
2025-11-21 08:49:16 +0200

    feat: Implement paginator for posts

--- pw5/myblog/blog/views.py
+++ pw5/myblog/blog/views.py
@@ -1,10 +1,16 @@
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Post
 
 # Create your views here.
 def post_list(request):
-    posts = Post.published.all()
+    posts_list = Post.published.all()
+    paginator = Paginator(posts_list, 3)
+    page_number = request.GET.get('page', 1)
+    posts = paginator.page(page_number)
+
     return render(request, 'blog/post/list.html', {'posts': posts})
+
 
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(
