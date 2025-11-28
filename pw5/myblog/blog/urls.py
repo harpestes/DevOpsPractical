58df9d780dae6634d5720f7a25d8718762e2dd4b
harpestes <dikaloandrij@gmail.com>
2025-11-28 10:23:50 +0200

    feat: Implement posts search

--- pw5/myblog/blog/urls.py
+++ pw5/myblog/blog/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('<int:post_id>/comment/', views.post_comment, name='post_comment'),
     path('tag/<slug:tag_slug>/', views.PostListView.as_view(), name='post_list_by_tag'),
     path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('search/', views.post_search, name='post_search'),
 ]
